951a6a1919fc85c296f2b1741f1d0548f04b38a5
citrabot <citra@citra-emu.org>
2023-06-21 04:30:18 +0000

    Merge PR 6600

--- src/common/settings.h
+++ src/common/settings.h
@@ -34,8 +34,9 @@ enum class LayoutOption : u32 {
     LargeScreen,
     SideScreen,
 #ifndef ANDROID
-    SeparateWindows,
+        SeparateWindows,
 #endif
+    HybridScreen,
     // Similiar to default, but better for mobile devices in portrait mode. Top screen in clamped to
     // the top of the frame, and the bottom screen is enlarged to match the top screen.
     MobilePortrait,
